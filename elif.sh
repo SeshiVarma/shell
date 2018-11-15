3941296ee8efd481f8ce7b0a83d7b608ac8debd1
malli <sshvarma2016@gmail.com>
2018-11-15 09:23:50 +0530

    this is the elif and ifcase

---
+++ elif.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+word=c
+if [[ $word == b ]]
+then
+	echo "condition b is true"
+elif [[ $word == a ]]
+then
+	echo "condition a is true"
+else 
+	echo "condition is fasle"
+fi
---
+++ ifcase.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+Count=10
+if [ $Count -eq 9 ]
+then
+	echo "statement is true"
+else
+	echo "statement is not true"
+fi
